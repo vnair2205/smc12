be66aa3eae6a2e5d4ccb207dfdf91aaf43205220
vnair2205 <vnair2205@gmail.com>
2025-09-09 13:56:51 +0530

    jg

---
+++ server/models/Admin.js
@@ -0,0 +1,30 @@
+// server/models/Admin.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const AdminSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
+});
+
+// Hash password before saving
+AdminSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+module.exports = mongoose.model('Admin', AdminSchema);
